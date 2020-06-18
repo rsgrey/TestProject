ed51e2f3891a34fc1cdd3cf5be918b877c489232
Reitenbach Sergey <sreitenbach@itera.kz>
2020-06-18 11:42:50 +0600

    Добавлено представление списка и адаптер для него

--- TestProject/MainActivity.cs
+++ TestProject/MainActivity.cs
@@ -4,56 +4,44 @@ using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
+using Android.Support.V7.Widget;
 using Android.Views;
 using Android.Widget;
 
 namespace TestProject
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+            RecyclerView recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            recyclerView.SetAdapter(new MyAdapter());
+        }
+    }
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+    public class MyAdapter : RecyclerView.Adapter
+    {
+        public override int ItemCount => 10;
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+
         }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
+            return new CustomViewHolder(LayoutInflater.From(parent.Context).Inflate(Resource.Layout.item_content, parent, false));
         }
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
+        public class CustomViewHolder : RecyclerView.ViewHolder
         {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            public CustomViewHolder(View itemView) : base(itemView)
             {
-                return true;
             }
-
-            return base.OnOptionsItemSelected(item);
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-	}
+    }
 }
